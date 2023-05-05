6b5c47e0a7412f8b68e9548a4f1fe2e14d7a096e
Levi Lutz <levi6900@icloud.com>
2023-05-05 14:10:00 -0400

    Switch to http instead of raw TCP

--- src/main.go
+++ src/main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
-	"net"
+	"io"
+	"net/http"
 	"os"
-	"strings"
 )
 
 const Version = "0.1.0"
-
-func checkErr(err error) {
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-}
 
 func getCLIArgs() (string, *string) {
 	if len(os.Args) == 1 {
@@ -26,6 +19,14 @@ func getCLIArgs() (string, *string) {
 	return os.Args[1], &os.Args[2]
 }
 
+func getPing(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "pong")
+}
+
+func getVersion(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, Version)
+}
+
 func main() {
 	selfPort, seedPartnerAddr := getCLIArgs()
 
@@ -34,57 +35,16 @@ func main() {
 		neighbors[0] = *seedPartnerAddr
 	}
 
-	listen, err := net.Listen("tcp", "0.0.0.0:"+selfPort)
-	checkErr(err)
-	fmt.Printf("Up at 0.0.0.0:%s\n", selfPort)
+	http.HandleFunc("/ping", getPing)
+	http.HandleFunc("/version", getVersion)
 
-	defer listen.Close()
-	for {
-		conn, err := listen.Accept()
-		checkErr(err)
-		go dispatchRequest(conn)
-	}
-}
+	fmt.Printf("Starting at 0.0.0.0:%s\n", selfPort)
+	err := http.ListenAndServe("0.0.0.0:"+selfPort, nil)
 
-func pingHandler(conn net.Conn, lines []string) {
-	conn.Write([]byte("pong"))
-	conn.Close()
-}
-
-func versionHandler(conn net.Conn, lines []string) {
-	if len(lines) < 2 || len(lines[1]) == 0 {
-		conn.Close()
-		fmt.Println("No version given")
-		return
-	}
-	theirVersion := lines[1]
-	// Send verack
-	conn.Write([]byte("verack\n" + theirVersion))
-	conn.Close()
-}
-
-func fallbackHandler(conn net.Conn, lines []string) {
-	conn.Write([]byte("UNKNOWN"))
-	conn.Close()
-}
-
-func dispatchRequest(conn net.Conn) {
-	buffer := make([]byte, 128)
-	_, err := conn.Read(buffer)
-	checkErr(err)
-
-	recv := strings.TrimRight(string(buffer), "\x00")
-	lines := strings.Split(recv, "\n")
-
-	dispatch := map[string]func(conn net.Conn, lines []string){
-		"ping":    pingHandler,
-		"version": versionHandler,
-	}
-
-	handler, ok := dispatch[lines[0]]
-	if ok {
-		handler(conn, lines)
-	} else {
-		fallbackHandler(conn, lines)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Println("Server closed")
+	} else if err != nil {
+		fmt.Fprintf(os.Stderr, "error starting server: %v\n", err)
+		os.Exit(1)
 	}
 }
