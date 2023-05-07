3e4447dfcdb9812feb6613886a7f3f80bcd46d01
Levi Lutz <levi6900@icloud.com>
2023-05-06 22:51:17 -0400

    Update peer versions in loop

--- src/main.go
+++ src/main.go
@@ -50,11 +50,47 @@ func (pc *PeersContainer) Get(addr string) (record PeerRecord) {
 	return
 }
 
+func (pc *PeersContainer) GetAddrs() (addrs []string) {
+	pc.mu.Lock()
+	defer pc.mu.Unlock()
+	addrs = make([]string, len(pc.peers))
+	i := 0
+	for k := range pc.peers {
+		addrs[i] = k
+		i++
+	}
+	return
+}
+
 func (pc *PeersContainer) Print() {
 	pc.mu.Lock()
 	defer pc.mu.Unlock()
 	for addr, record := range pc.peers {
 		fmt.Printf("%s\t%d\t%v\n", addr, record.LastUpdated.Unix(), record.Version)
+	}
+}
+
+func updatePeerVersion(peers *PeersContainer, addr string) error {
+	resp, err := utils.RetryGetBody[VersionResp]("http://"+addr+"/version", 3)
+	if err != nil {
+		return err
+	}
+	peers.Upsert(addr, *resp)
+	return nil
+}
+
+func updatePeerLoop(peers *PeersContainer, interval int, kill <-chan bool) {
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
+	for {
+		select {
+		case <-kill:
+			return
+		case <-ticker.C:
+			addrs := peers.GetAddrs()
+			for _, addr := range addrs {
+				go updatePeerVersion(peers, addr)
+			}
+		}
 	}
 }
 
@@ -87,14 +123,14 @@ func main() {
 	peers := NewPeersContainer()
 
 	if *seedAddr != "" {
-		resp, err := utils.RetryGetBody[VersionResp]("http://"+*seedAddr+"/version", 3)
+		err := updatePeerVersion(peers, *seedAddr)
 		if err != nil {
 			fmt.Printf("Failed to connect to seed peer: %s", err)
 			os.Exit(1)
 		}
-		peers.Upsert(*seedAddr, *resp)
 	}
 	peers.Print()
+	go updatePeerLoop(peers, 15, nil)
 
 	http.HandleFunc("/ping", getPing)
 	http.HandleFunc("/version", getVersion)
