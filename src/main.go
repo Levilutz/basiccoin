eda11e8fcb880989ec4609f1ca74031351bea2f7
Levi Lutz <levi6900@icloud.com>
2023-05-09 23:12:44 -0400

    Blank slate

--- src/main.go
+++ src/main.go
@@ -1,55 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"net/http"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	"github.com/levilutz/basiccoin/src/p2p"
-	"github.com/levilutz/basiccoin/src/utils"
-)
-
-func getCLIArgs() (localAddr, seedAddr *string) {
-	localAddr = flag.String(
-		"localAddr", "0.0.0.0:21720", "Local address to host server",
-	)
-	seedAddr = flag.String(
-		"seedAddr", "", "Seed partner, or nothing to create new network",
-	)
-	flag.Parse()
-	return
-}
-
-func getPing(c *gin.Context) {
-	c.String(http.StatusOK, "pong")
-}
+import "fmt"
 
 func main() {
-	localAddr, seedAddr := getCLIArgs()
-	utils.Constants.LocalAddr = *localAddr
-
-	pn := p2p.NewP2pNetwork()
-
-	// Connect to seed if given one
-	if *seedAddr != "" {
-		err := pn.RetryAddPeer(*seedAddr, true)
-		if err != nil {
-			fmt.Printf("failed to connect to seed peer: %s", err)
-			os.Exit(1)
-		}
-		go pn.Expand()
-	}
-	pn.Print()
-	go pn.SyncLoop(false, nil)
-
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	r.GET("/ping", getPing)
-	p2p.Mount(r, pn)
-
-	fmt.Printf("starting at %s\n", *localAddr)
-	r.SetTrustedProxies(nil)
-	r.Run(*localAddr)
+	fmt.Println("starting...")
 }
