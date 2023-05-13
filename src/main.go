13fe578871c22de818fd8cc53ef9659a53cccc07
Levi Lutz <levi6900@icloud.com>
2023-05-13 02:17:02 -0400

    Have CLI set Constants directly

--- src/main.go
+++ src/main.go
@@ -1,49 +1,18 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"net"
-	"time"
 
 	"github.com/levilutz/basiccoin/src/util"
 )
 
 func main() {
-	cli_args := GetCLIArgs()
+	cli_args := util.ParseCLIArgs()
 	util.PrettyPrint(cli_args)
 	util.PrettyPrint(util.Constants)
 
-	done := make(chan bool)
-
-	go func() {
-		addr, _ := net.ResolveTCPAddr("tcp", "0.0.0.0:21720")
-		listen, _ := net.ListenTCP("tcp", addr)
-		defer listen.Close()
-		conn, _ := listen.Accept()
-		defer conn.Close()
-		reader := bufio.NewReader(conn)
-		msg, err := ReceiveHelloMessage(reader, false)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			util.PrettyPrint(msg)
-		}
-		done <- true
-	}()
-
-	time.Sleep(time.Second)
-
-	addr, _ := net.ResolveTCPAddr("tcp", "0.0.0.0:21720")
-	conn, _ := net.DialTCP("tcp", nil, addr)
-	writer := bufio.NewWriter(conn)
-	msg := HelloMessage{
-		RuntimeID: util.Constants.RuntimeID,
-		Version:   util.Constants.Version,
-		Addr:      "",
+	_, err := net.ResolveTCPAddr("tcp", cli_args.LocalAddr)
+	if err != nil {
+		panic(err)
 	}
-	msg.Transmit(writer)
-	conn.Close()
-
-	<-done
 }
---
+++ src/util/cli.go
@@ -0,0 +1,28 @@
+package util
+
+import "flag"
+
+type CLIArgs struct {
+	LocalAddr string `json:"localAddr"`
+	SeedAddr  string `json:"seedAddr"`
+}
+
+func ParseCLIArgs() CLIArgs {
+	// Define args
+	localAddr := flag.String("addr", "", "Address to host from")
+	seedAddr := flag.String("seed", "", "Seed peer, or nothing to create new network")
+
+	// Do the parse
+	flag.Parse()
+
+	// Insert into Constants
+	if *localAddr != "" {
+		Constants.LocalAddr = *localAddr
+	}
+
+	// Return all (even those in constants)
+	return CLIArgs{
+		LocalAddr: *localAddr,
+		SeedAddr:  *seedAddr,
+	}
+}
