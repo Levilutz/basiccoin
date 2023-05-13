7bbd5dfcf4ff5278065eb0ada206555ec302e039
Levi Lutz <levi6900@icloud.com>
2023-05-13 01:55:35 -0400

    Generalize standard messages

--- src/message.go
+++ src/message.go
@@ -7,6 +7,33 @@ import (
 	"github.com/levilutz/basiccoin/src/util"
 )
 
+// Generic helpers
+
+// Receive messageName then base64(json(message)) in a single line
+func ReceiveSingleStandardMessage[R any](
+	messageName string,
+	r *bufio.Reader,
+	nameConsumed bool,
+) (R, error) {
+	var content R
+	if !nameConsumed {
+		data, err := util.RetryReadLine(r, 5)
+		if err != nil {
+			return content, err
+		}
+		if string(data) != HelloMessageName {
+			return content, fmt.Errorf(
+				"expected '%s', received '%s'", messageName, string(data),
+			)
+		}
+	}
+	data, err := util.RetryReadLine(r, 5)
+	if err != nil {
+		return content, err
+	}
+	return util.UnJsonB64[R](data)
+}
+
 // HelloMessage
 
 var HelloMessageName = "hello"
@@ -19,26 +46,9 @@ type HelloMessage struct {
 
 // Receive the HelloMessage from the channel, consuming name if not done yet
 func ReceiveHelloMessage(r *bufio.Reader, nameConsumed bool) (HelloMessage, error) {
-	if !nameConsumed {
-		data, err := util.RetryReadLine(r, 5)
-		if err != nil {
-			return HelloMessage{}, err
-		}
-		if string(data) != HelloMessageName {
-			return HelloMessage{}, fmt.Errorf(
-				"expected '%s', received '%s'", HelloMessageName, string(data),
-			)
-		}
-	}
-	data, err := util.RetryReadLine(r, 5)
-	if err != nil {
-		return HelloMessage{}, err
-	}
-	content, err := util.UnJsonB64[HelloMessage](data)
-	if err != nil {
-		return HelloMessage{}, err
-	}
-	return content, nil
+	return ReceiveSingleStandardMessage[HelloMessage](
+		HelloMessageName, r, nameConsumed,
+	)
 }
 
 // Transmit the HelloMessage over the channel, including name
