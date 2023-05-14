87820ae57115e847fb4781776a19cd1b46231c8d
Levi Lutz <levi6900@icloud.com>
2023-05-14 17:28:11 -0400

    Handle simultaneous commands between peers

--- src/peer/peer.go
+++ src/peer/peer.go
@@ -1,6 +1,7 @@
 package peer
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"time"
@@ -11,22 +12,27 @@ import (
 
 // Encapsulate a high-level connection to a peer.
 type Peer struct {
-	HelloMsg *HelloPeerMessage
-	EventBus chan events.PeerEvent
-	conn     *PeerConn
-	mainBus  chan<- events.MainEvent
+	HelloMsg       *HelloPeerMessage
+	EventBus       chan events.PeerEvent
+	conn           *PeerConn
+	mainBus        chan<- events.MainEvent
+	weAreInitiator bool
 }
 
 // Create a Peer from the result of a successfull handshake on a PeerConn, the
 // associated PeerConn, and a bus to emit events back to the manager loop.
 func NewPeer(
-	msg *HelloPeerMessage, pc *PeerConn, mainBus chan events.MainEvent,
+	msg *HelloPeerMessage,
+	pc *PeerConn,
+	mainBus chan events.MainEvent,
+	weAreInitiator bool,
 ) *Peer {
 	return &Peer{
-		HelloMsg: msg,
-		EventBus: make(chan events.PeerEvent, util.Constants.PeerBusBufferSize),
-		conn:     pc,
-		mainBus:  mainBus,
+		HelloMsg:       msg,
+		EventBus:       make(chan events.PeerEvent, util.Constants.PeerBusBufferSize),
+		conn:           pc,
+		mainBus:        mainBus,
+		weAreInitiator: weAreInitiator,
 	}
 }
 
@@ -44,7 +50,7 @@ func NewPeerOutbound(addr string, mainBus chan events.MainEvent) (*Peer, error) 
 		return nil, err
 	}
 
-	return NewPeer(helloMsg, pc, mainBus), nil
+	return NewPeer(helloMsg, pc, mainBus, true), nil
 }
 
 // Attempt to initialize a new inbound connection given the TCP Conn.
@@ -58,55 +64,119 @@ func NewPeerInbound(conn *net.TCPConn, mainBus chan events.MainEvent) (*Peer, er
 		return nil, err
 	}
 
-	return NewPeer(helloMsg, pc, mainBus), nil
+	return NewPeer(helloMsg, pc, mainBus, false), nil
 }
 
 // Loop handling events from our message bus and the peer
 func (p *Peer) Loop() {
+	var err error
 	listenTicker := time.NewTicker(util.Constants.PeerListenFreq)
 	pingTicker := time.NewTicker(util.Constants.PeerPingFreq)
 	for {
+		shouldClose := false
 		select {
 		case event := <-p.EventBus:
-			p.handlePeerBusEvent(event)
+			shouldClose = p.handlePeerBusEvent(event)
 		case <-listenTicker.C:
 			line := p.conn.ReadLineTimeout(25)
 			if p.conn.Err() != nil {
 				continue
 			}
-			p.handleReceivedCommand(string(line))
+			shouldClose, err = p.handleReceivedLine(line)
+			if err != nil {
+				fmt.Printf("error handling '%s': %s\n", line, err.Error())
+			}
 		case <-pingTicker.C:
-			p.conn.TransmitStringLine("cmd:ping")
-			p.conn.ConsumeExpected("ack:ping")
-			if err := p.conn.Err(); err == nil {
+			shouldClose, err = p.issuePeerCommand("ping", func() error {
+				return nil
+			})
+			if err == nil {
 				fmt.Println("ping success")
 			} else {
 				fmt.Println("ping err", err.Error())
 			}
 		}
+		if shouldClose {
+			return
+		}
 	}
 }
 
-// Handle event from our message bus
-func (p *Peer) handlePeerBusEvent(event events.PeerEvent) {
-	fmt.Println(event)
+func (p *Peer) handleClose() error {
+	p.conn.TransmitStringLine("ack:close")
+	p.mainBus <- events.MainEvent{
+		PeerClosing: &events.PeerClosingMainEvent{
+			RuntimeID: p.HelloMsg.RuntimeID,
+		},
+	}
+	return p.conn.Err()
 }
 
-// Handle command received from peer
-func (p *Peer) handleReceivedCommand(command string) {
-	if command == "cmd:close" {
-		p.conn.TransmitStringLine("ack:close")
-		p.mainBus <- events.MainEvent{
-			PeerClosing: &events.PeerClosingMainEvent{
-				RuntimeID: p.HelloMsg.RuntimeID,
-			},
-		}
-		return
+// Handle event from our message bus, return whether we should close.
+func (p *Peer) handlePeerBusEvent(event events.PeerEvent) bool {
+	fmt.Println(event)
+	return false
+}
 
-	} else if command == "cmd:ping" {
+// Issue an outbound interaction for the given command (without "cmd:").
+// Handler is what to run after they ack. Returns whether we should close.
+// If us and peer simultaneously issued commands, the conn initiator goes second.
+func (p *Peer) issuePeerCommand(command string, handler func() error) (bool, error) {
+	p.conn.TransmitStringLine("cmd:" + command)
+	resp := p.conn.RetryReadLine(7)
+	if err := p.conn.Err(); err != nil {
+		return false, err
+	}
+	if string(resp) == "ack:"+command {
+		return false, handler()
+	}
+	if bytes.HasPrefix(resp, []byte("cmd:")) {
+		if string(resp) == "cmd:close" {
+			// Handle close immediately no matter who is the initiator
+			return true, p.handleClose()
+		} else if p.weAreInitiator {
+			// Handle their request, then expect ours to be handled
+			shouldClose, err := p.handleReceivedLine(resp)
+			if shouldClose || err != nil {
+				return shouldClose, err
+			}
+			p.conn.ConsumeExpected("ack:" + command)
+			if err := p.conn.Err(); err != nil {
+				return false, err
+			}
+			return false, handler()
+		} else {
+			// Expect them to handle our request, then theirs
+			p.conn.ConsumeExpected("ack:" + command)
+			if err := p.conn.Err(); err != nil {
+				return false, err
+			}
+			err := handler()
+			if err != nil {
+				return false, err
+			}
+			p.handleReceivedLine(resp)
+		}
+	}
+	return false, nil
+}
+
+// Handle command received from peer, returns whether we should close.
+func (p *Peer) handleReceivedLine(line []byte) (bool, error) {
+	if !bytes.HasPrefix(line, []byte("cmd:")) {
+		return false, fmt.Errorf("unrecognized line: %s", line)
+	}
+	command := string(line)[4:]
+	// TODO ack before handling
+	if command == "close" {
+		return true, p.handleClose()
+
+	} else if command == "ping" {
 		p.conn.TransmitStringLine("ack:ping")
+		return false, p.conn.Err()
 
 	} else {
 		fmt.Println("Unexpected peer message:", command)
+		return false, nil
 	}
 }
