60d2a7e9e31ba5942ba27ae8f0483ea900badfbf
Levi Lutz <levi6900@icloud.com>
2023-05-26 07:26:57 -0400

    Remove verify while re-evaluating db structure

--- src/db/db.go
+++ src/db/db.go
@@ -12,12 +12,16 @@ func (utxo UTXO) Hash() HashT {
 
 // Total state of the blockchain and mempool.
 type State struct {
-	head    HashT
-	ledger  map[HashT]BlockHeader
-	merkles map[HashT]MerkleNode
-	txs     map[HashT]Tx
-	utxos   map[HashT]struct{}
-	mempool map[HashT]struct{}
+	// Mutable
+	head     HashT
+	mempool  map[HashT]struct{}
+	curUtxos map[HashT]struct{}
+	// Write-once maps
+	ledger    map[HashT]BlockHeader
+	blockHs   map[HashT]uint32
+	merkles   map[HashT]MerkleNode
+	txs       map[HashT]Tx
+	usedUtxos map[HashT]HashT
 }
 
 // Add a tx to the mempool, fails if it exists in ledger.
@@ -30,11 +34,15 @@ func (s *State) AddMempoolTx(hash HashT, tx Tx) error {
 	return nil
 }
 
-func (s *State) UtxoExists(utxo UTXO, blockId HashT) bool {
-	if blockId == s.head {
-		_, ok := s.utxos[utxo.Hash()]
-		return ok
-	}
-	// TODO: Rewind
-	return false
-}
+// Whether the given UTXO exists at the conclusion of the given block id.
+// func (s *State) UtxoExists(utxo UTXO, blockId HashT) bool {
+// 	usedBlock, ok := s.utxos[utxo.Hash()]
+// 	if !ok {
+// 		return false
+// 	}
+// 	if usedBlock == BlankHash && blockId == s.head {
+// 		return true
+// 	}
+// 	// Whether usedHeight > blockHeight
+// 	return s.blockHs[usedBlock] > s.blockHs[blockId]
+// }
