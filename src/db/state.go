694f39acc1b81bb2c07a669ac6c14f3d2362dd79
Levi Lutz <levi6900@icloud.com>
2023-05-28 17:42:30 -0700

    State Rewind

--- src/db/state.go
+++ src/db/state.go
@@ -8,7 +8,15 @@ type Utxo struct {
 	Ind  uint32
 }
 
+func UtxoFromInput(txi TxIn) Utxo {
+	return Utxo{
+		TxId: txi.OriginTxId,
+		Ind:  txi.OriginTxOutInd,
+	}
+}
+
 // State at a blockchain node.
+// Meant to only be accessed synchronously by a single thread.
 type State struct {
 	Head    HashT
 	Mempool *util.Set[HashT]
@@ -27,7 +35,27 @@ func (s *State) Copy() *State {
 }
 
 // Rewind a state to its parent block.
+// If this fails state will be corrupted, so copy before if necessary.
 func (s *State) Rewind() error {
+	rBlock, _, rTxs, err := s.inv.LoadFullBlock(s.Head)
+	if err != nil {
+		return err
+	}
+	for txId, tx := range rTxs {
+		// Return tx back to mempool
+		s.Mempool.Add(txId)
+		// Return the tx inputs
+		for _, txi := range tx.Inputs {
+			s.Utxos.Add(UtxoFromInput(txi))
+		}
+		// Remove the tx outputs from the utxo set
+		for i, _ := range tx.Outputs {
+			if !s.Utxos.Remove(Utxo{TxId: txId, Ind: uint32(i)}) {
+				return err
+			}
+		}
+	}
+	s.Head = rBlock.PrevBlockId
 	return nil
 }
 
@@ -37,6 +65,7 @@ func (s *State) CanAdvance(next Block) error {
 }
 
 // Advance a state to the next block, does not verify.
+// If this fails state will be corrupted, so copy before if necessary.
 func (s *State) Advance(next Block) error {
 	return nil
 }
--- src/util/set.go
+++ src/util/set.go
@@ -5,21 +5,34 @@ type Set[K comparable] struct {
 	s map[K]struct{}
 }
 
+// Create a new set.
 func NewSet[K comparable]() *Set[K] {
 	return &Set[K]{
 		s: make(map[K]struct{}),
 	}
 }
 
+// Add a key to the set.
 func (s *Set[K]) Add(key K) {
 	s.s[key] = struct{}{}
 }
 
+// Remove a key from the set, return whether it existed.
+func (s *Set[K]) Remove(key K) bool {
+	if !s.Includes(key) {
+		return false
+	}
+	delete(s.s, key)
+	return true
+}
+
+// Check whether a set includes the given item.
 func (s *Set[K]) Includes(key K) bool {
 	_, ok := s.s[key]
 	return ok
 }
 
+// Shallow copy the set.
 func (s *Set[K]) Copy() *Set[K] {
 	newS := make(map[K]struct{}, len(s.s))
 	for k := range s.s {
