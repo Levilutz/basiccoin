f39883b23bc18224079fa7425005d4c774f09619
Levi Lutz <levi6900@icloud.com>
2023-06-04 11:10:19 -0700

    Move http handlers into separate file

--- src/client/client.go
+++ src/client/client.go
@@ -1,11 +1,8 @@
 package client
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"strconv"
 
 	"github.com/levilutz/basiccoin/src/db"
 	"github.com/levilutz/basiccoin/src/util"
@@ -16,58 +13,11 @@ type MainQueryHandler interface {
 }
 
 func Start(m MainQueryHandler) {
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "pong")
-	})
+	handler := Handler{m: m}
 
-	http.HandleFunc("/balance", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "GET" {
-			w.WriteHeader(405)
-			io.WriteString(w, "method not allowed: "+r.Method)
-			return
-		}
-		publicKeyHashes, ok := r.URL.Query()["publicKeyHash"]
-		if !ok || len(publicKeyHashes) < 1 {
-			w.WriteHeader(400)
-			io.WriteString(w, "must provide public key hash")
-			return
-		}
-		pkh, err := db.StringToHash(publicKeyHashes[0])
-		if err != nil {
-			w.WriteHeader(400)
-			io.WriteString(w, err.Error())
-			return
-		}
-		rCh := make(chan uint64)
-		m.HandleBalanceQuery(rCh, pkh)
-		resp := <-rCh
-		io.WriteString(w, strconv.FormatUint(resp, 10))
-	})
-
-	http.HandleFunc("/tx", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != "POST" {
-			w.WriteHeader(405)
-			io.WriteString(w, "method not allowed: "+r.Method)
-			return
-		}
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(400)
-			io.WriteString(w, "failed to read body: "+err.Error())
-			return
-		}
-		tx := db.Tx{}
-		if err = json.Unmarshal(body, &tx); err != nil {
-			w.WriteHeader(422)
-			io.WriteString(w, "failed to parse json: "+err.Error())
-			return
-		}
-		if !tx.HasSurplus() {
-			w.WriteHeader(400)
-			io.WriteString(w, "tx without surplus would never be included")
-			return
-		}
-	})
+	http.HandleFunc("/ping", handler.handleGetPing)
+	http.HandleFunc("/balance", handler.handleBalance)
+	http.HandleFunc("/tx", handler.handleTx)
 
 	portStr := fmt.Sprintf(":%d", util.Constants.HttpPort)
 	http.ListenAndServe(portStr, nil)
---
+++ src/client/handlers.go
@@ -0,0 +1,84 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/levilutz/basiccoin/src/db"
+)
+
+type Handler struct {
+	m MainQueryHandler
+}
+
+func (h *Handler) handleGetPing(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "pong")
+}
+
+func (h *Handler) handleBalance(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		h.handleGetBalance(w, r)
+	} else {
+		write405(w, r)
+	}
+}
+
+func (h *Handler) handleGetBalance(w http.ResponseWriter, r *http.Request) {
+	publicKeyHashes, ok := r.URL.Query()["publicKeyHash"]
+	if !ok || len(publicKeyHashes) < 1 {
+		write400(w, r, fmt.Errorf("must provide public key hash"))
+		return
+	}
+	pkh, err := db.StringToHash(publicKeyHashes[0])
+	if err != nil {
+		write400(w, r, err)
+		return
+	}
+	rCh := make(chan uint64)
+	h.m.HandleBalanceQuery(rCh, pkh)
+	resp := <-rCh
+	io.WriteString(w, strconv.FormatUint(resp, 10))
+}
+
+func (h *Handler) handleTx(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		h.handlePostTx(w, r)
+	} else {
+		write405(w, r)
+	}
+}
+
+func (h *Handler) handlePostTx(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		write400(w, r, err)
+		return
+	}
+	tx := db.Tx{}
+	if err = json.Unmarshal(body, &tx); err != nil {
+		write422(w, r, err)
+		return
+	}
+	if !tx.HasSurplus() {
+		write400(w, r, fmt.Errorf("tx without surplus would never be included"))
+		return
+	}
+}
+
+func write400(w http.ResponseWriter, r *http.Request, err error) {
+	w.WriteHeader(400)
+	io.WriteString(w, "client error: "+err.Error())
+}
+
+func write422(w http.ResponseWriter, r *http.Request, err error) {
+	w.WriteHeader(422)
+	io.WriteString(w, "failed to parse: "+err.Error())
+}
+
+func write405(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(405)
+	io.WriteString(w, "method not allowed: "+r.Method)
+}
