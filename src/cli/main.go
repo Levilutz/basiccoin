e23084427ecf9d9dfaedd2a42291924e6a2333eb
Levi Lutz <levi6900@icloud.com>
2023-06-06 20:10:29 -0400

    New cli commands approach

--- src/cli/main.go
+++ src/cli/main.go
@@ -5,6 +5,71 @@ import (
 	"os"
 )
 
+type Command struct {
+	HelpText       string
+	UsageText      string
+	RequiredArgs   int
+	RequiresConfig bool
+	Handler        func(args []string, cfg *Config) error
+}
+
+var commands map[string]Command = map[string]Command{
+	"setup": {
+		HelpText:       "Set up the local wallet instance.",
+		UsageText:      "usage: basiccoin-cli setup",
+		RequiredArgs:   0,
+		RequiresConfig: false,
+		Handler: func(args []string, cfg *Config) error {
+			return nil
+		},
+	},
+	"import": {
+		HelpText:       "Import the given file into the current wallet.",
+		UsageText:      "usage: basiccoin-cli import [path]",
+		RequiredArgs:   0,
+		RequiresConfig: false,
+		Handler: func(args []string, cfg *Config) error {
+			return nil
+		},
+	},
+	"balance": {
+		HelpText:       "Get the total balance of all currently controlled addresses, or a given address.",
+		UsageText:      "usage: basiccoin-cli balance (address)",
+		RequiredArgs:   0,
+		RequiresConfig: true,
+		Handler: func(args []string, cfg *Config) error {
+			return nil
+		},
+	},
+	"send": {
+		HelpText:       "Send coin to a given address.",
+		UsageText:      "usage: basiccoin-cli send [address] [amount]",
+		RequiredArgs:   0,
+		RequiresConfig: true,
+		Handler: func(args []string, cfg *Config) error {
+			return nil
+		},
+	},
+	"history": {
+		HelpText:       "Get the history of all currently controlled addresses, or a given address.",
+		UsageText:      "usage: basiccoin-cli history (address)",
+		RequiredArgs:   0,
+		RequiresConfig: true,
+		Handler: func(args []string, cfg *Config) error {
+			return nil
+		},
+	},
+	"get-config-path": {
+		HelpText:       "Print the path to our current config file.",
+		UsageText:      "usage: basiccoin-cli config-dir",
+		RequiredArgs:   0,
+		RequiresConfig: true,
+		Handler: func(args []string, cfg *Config) error {
+			return nil
+		},
+	},
+}
+
 func main() {
 	cfg := GetConfig()
 
@@ -16,54 +81,64 @@ func main() {
 	command := os.Args[1]
 	cmdArgs := os.Args[2:]
 
-	// Show help message if wanted
+	// Show general help message if wanted
 	if command == "help" {
 		PrintGeneralHelp()
 		return
-	} else if len(cmdArgs) > 0 && cmdArgs[0] == "help" {
-		PrintCommandHelp(command)
+	}
+
+	cmd, ok := commands[command]
+	if !ok {
+		fmt.Println(yellowStr("command not found"))
 		return
 	}
 
-	// Commands that don't require config
-	if command == "setup" {
-		fmt.Println(greenStr("success"))
+	// Show command help message if wanted
+	if len(cmdArgs) > 0 && cmdArgs[0] == "help" {
+		fmt.Println(cmd.HelpText)
+		fmt.Println(cmd.UsageText)
 		return
 	}
 
-	if command == "import" {
-		if len(cmdArgs) < 1 {
-			fmt.Println(yellowStr("insufficient arguments"))
-			return
-		}
-		fmt.Println(greenStr("success"))
+	// Verify sufficient arguments
+	if len(cmdArgs) < cmd.RequiredArgs {
+		fmt.Println(yellowStr("insufficient arguments"))
+		fmt.Println(cmd.UsageText)
 		return
 	}
 
-	if cfg == nil {
+	// Verify configured if required
+	if cfg == nil && cmd.RequiresConfig {
 		fmt.Println(yellowStr("command requires setup, run 'basiccoin-cli setup' first"))
 		return
 	}
 
-	// Commands that require config
-	if command == "balance" {
-		return
-
-	} else if command == "send" {
-		if len(cmdArgs) < 2 {
-			fmt.Println(yellowStr("insufficient arguments"))
-			return
-		}
-		fmt.Println(greenStr("success"))
-		return
-
-	} else if command == "history" {
-		return
-
-	} else if command == "config-dir" {
-		return
-
+	// Run the command
+	err := cmd.Handler(cmdArgs, cfg)
+	if err != nil {
+		fmt.Println(redStr(err.Error()))
 	} else {
-		fmt.Println(redStr("command not found:"), command)
+		fmt.Println(greenStr("success"))
 	}
+}
+
+func PrintGeneralHelp() {
+	fmt.Println("Query a basiccoin node and manage a wallet.")
+	fmt.Println("usage: basiccoin-cli [command] ...")
+	fmt.Println("available commands:")
+	for cmd := range commands {
+		fmt.Printf("\t%s\n", cmd)
+	}
+}
+
+func greenStr(str string) string {
+	return fmt.Sprintf("\033[32m%s\033[0m", str)
+}
+
+func yellowStr(str string) string {
+	return fmt.Sprintf("\033[33m%s\033[0m", str)
+}
+
+func redStr(str string) string {
+	return fmt.Sprintf("\033[31m%s\033[0m", str)
 }
