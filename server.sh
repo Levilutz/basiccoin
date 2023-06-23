ff207b5bbeb239e8a26ae9046256707fe49b7a2a
Levi Lutz <levi6900@icloud.com>
2023-06-23 19:51:47 -0400

    Startup script

---
+++ server.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Install and run a basiccoin node on a linux machine
+
+set -ex
+
+# Get curl and git
+sudo apt-get update
+sudo apt-get install -y curl git
+
+# Get golang
+curl -LO https://go.dev/dl/go1.20.5.linux-amd64.tar.gz
+tar xvf go1.20.5.linux-amd64.tar.gz
+sudo chown -R root:root ./go
+sudo mv go /usr/local
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
+# shellcheck disable=SC1090
+source ~/.bashrc
+
+# Get basiccoin
+git clone https://github.com/Levilutz/basiccoin.git
+cd basiccoin
+bash build.sh
+
+# Run server
+nohup ./bcnode \
+    --addr="$ADDR" \
+    --miners=1 \
+    --payout=eeeeeed6bdacc4d88f6e07ba9070a3bcc1d1648cb8393ecb47bbe02235e48a5a \
+    --http-wallet \
+    --listen=true \
+    > ~/basiccoin.out \
+    2> ~/basiccoin.err &
